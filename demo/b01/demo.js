3d24161a4f0f70a72b279dc6fd25e72ed208c2c4
Dinh-Bao Vu <vubao.edu@gmail.com>
2022-09-26 19:43:49 +0700

    update b01

--- demo/b01/demo.js
+++ demo/b01/demo.js
@@ -1,1 +1,10 @@
-console.log("bye");
+let x = Number(prompt("Nhập x:"));
+// let y = Number(x); // Chuyển x thành number
+
+// let x;
+// x = prompt("Nhập x: ");
+// let y;
+// y = Number(x);
+
+
+console.log(x + 1);
