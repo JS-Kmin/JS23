fecfd836cb2909a5dff7ee00a7da18abacfff636
Dinh-Bao Vu <vubao.edu@gmail.com>
2022-10-03 19:39:39 +0700

    b03

--- demo/b03/demo.js
+++ demo/b03/demo.js
@@ -1,8 +1,3 @@
-
-
-// Nếu x là số âm thì in ra là "Am",
-// Ngược lại thì in ra là "Không âm"
-
 // if (Điều kiện) {
 //     Lệnh sẽ làm nếu điều kiện đúng
 // }
@@ -14,11 +9,37 @@ function bai01() {
     let x = 4;
     if (x % 2 == 0) {
         console.log("Chan");
-        console.log("Hiii");
     }
     else {
         console.log("Le");
     }
 }
+function bai01_C2() {
+    let x = 4;
+    if (!(x % 2 == 0)) {
+        console.log("Le");
+    }
+    else {
+        console.log("Chan");
+    }
+}
 
-bai01();
+function tamGiacDeu() {
+    let a = 5, b = 5, c = 5;
+    if (a == b && b == c) {
+        console.log("TG deu");
+    }
+    else {
+        console.log("TG khong deu");
+    }
+}
+
+function tamGiacCan() {
+    let a = 5, b = 5, c = 5;
+    if (a == b || b == c || a == c) {
+        console.log("TG can");
+    }
+    else {
+        console.log("TG khong can");
+    }
+}
