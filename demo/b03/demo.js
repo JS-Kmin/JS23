ed4071dac19e00c3394b2e4f99c58f37ec4fb4c9
Dinh-Bao Vu <vubao.edu@gmail.com>
2022-10-03 21:01:00 +0700

    b03

--- demo/b03/demo.js
+++ demo/b03/demo.js
@@ -35,8 +35,8 @@ function tamGiacDeu() {
 }
 
 // De Morgan
-!(A && B)
-!A || !B
+// !(A && B)
+// !A || !B
 
 function tamGiacDeu2() {
     let a = 5, b = 5, c = 5;
@@ -94,4 +94,77 @@ function aSoB() {
             console.log("a bang b");
         }
     }
+}
+
+function soDienTieuThu() {
+    let x = Number(prompt("Nhập số kw điện"));
+    if (x < 100)
+        return x * 2000;
+    else if (x < 200)
+        return x * 2500;
+    else
+        return x * 3500;
+
+}
+// let y = soDienTieuThu();
+// console.log(y);
+
+function ptb1() {
+    let a = 5, b = 3;
+
+    if (a == 0)
+        if (b == 0)
+            console.log("VSN");
+        else
+            console.log("VN");
+    else
+        console.log("Nghiem:", -b / a);
+}
+
+function inSoAm() {
+    let a = -1, b = 2, c = -3;
+    if (a < 0)
+        console.log(a);
+    if (b < 0)
+        console.log(b);
+    if (c < 0)
+        console.log(c);
+}
+function tinhTongAm() {
+    let a = -1, b = 2, c = -3;
+
+    let s = 0;
+    if (a < 0)
+        s += a; // s = -1
+    if (b < 0)
+        s += b; // s = -1 + 2 = 1 (a+b)
+    if (c < 0)
+        s += c; // s = 1 + (-3) = -2 (a+b+c)
+
+    console.log(s);
+}
+
+function demSoAm() {
+    let a = -1, b = 2, c = -3;
+
+    let dem = 0;
+    if (a < 0)
+        dem++;
+    if (b < 0)
+        dem++;
+    if (c < 0)
+        dem++;
+
+    console.log(dem);
+}
+
+function maxNumber() {
+    let a = -1, b = 2, c = 0;
+
+    let max = a;
+
+    if (b > max)
+        max = b;
+    if (c > max)
+        max = c;
 }
