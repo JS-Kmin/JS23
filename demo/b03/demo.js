bf10db15215346e787c53af3e1c381f471880c54
Dinh-Bao Vu <vubao.edu@gmail.com>
2022-10-03 19:59:41 +0700

    b03

--- demo/b03/demo.js
+++ demo/b03/demo.js
@@ -34,6 +34,21 @@ function tamGiacDeu() {
     }
 }
 
+// De Morgan
+!(A && B)
+!A || !B
+
+function tamGiacDeu2() {
+    let a = 5, b = 5, c = 5;
+    if (a != b || b != c || c != a) {
+        console.log("TG khong deu");
+
+    }
+    else {
+        console.log("TG deu");
+    }
+}
+
 function tamGiacCan() {
     let a = 5, b = 5, c = 5;
     if (a == b || b == c || a == c) {
@@ -41,5 +56,42 @@ function tamGiacCan() {
     }
     else {
         console.log("TG khong can");
+    }
+}
+
+function tamGiacCan2() {
+    let a = 5, b = 5, c = 5;
+    if (a != b && b != c && a != c) {
+        console.log("TG khong can");
+    }
+    else {
+        console.log("TG can");
+    }
+}
+
+function tamGiacVuong() {
+    let a = 3, b = 4, c = 5;
+    if (a * a == b * b + c * c ||
+        b * b == a * a + c * c ||
+        c * c == a * a + b * b) {
+        console.log("TG vuong");
+    }
+    else {
+        console.log("TG khong vuong");
+    }
+}
+
+function aSoB() {
+    let a = 1, b = 2;
+    if (a > b) {
+        console.log("a lon hon b");
+    }
+    else {
+        if (a < b) {
+            console.log("a be hon b");
+        }
+        else {
+            console.log("a bang b");
+        }
     }
 }
