10aa286c2e3bd52b802544972933ebded1cb5ae2
Dinh-Bao Vu <vubao.edu@gmail.com>
2022-10-03 19:22:57 +0700

    b03

---
+++ demo/b03/demo.js
@@ -0,0 +1,24 @@
+
+
+// Nếu x là số âm thì in ra là "Am",
+// Ngược lại thì in ra là "Không âm"
+
+// if (Điều kiện) {
+//     Lệnh sẽ làm nếu điều kiện đúng
+// }
+// else {
+//     Lệnh sẽ làm nếu điều kiện sai
+// }
+
+function bai01() {
+    let x = 4;
+    if (x % 2 == 0) {
+        console.log("Chan");
+        console.log("Hiii");
+    }
+    else {
+        console.log("Le");
+    }
+}
+
+bai01();
