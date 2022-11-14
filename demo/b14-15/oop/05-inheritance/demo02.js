ff82512b902739154b02a7c446adbb5ea878f2d5
Dinh-Bao Vu <vubao.edu@gmail.com>
2022-11-14 20:58:35 +0700

    b15

--- demo/b14-15/oop/05-inheritance/demo02.js
+++ demo/b14-15/oop/05-inheritance/demo02.js
@@ -7,12 +7,13 @@ class Person {
         return this.firstName + ' ' + this.lastName;
     }
     print() {
-        console.log('***');
-        console.log(`- First name: ${this.firstName}`);
-        console.log(`- Last name: ${this.lastName}`);
+        console.log("Cha");
+        // console.log('***');
+        // console.log(`- First name: ${this.firstName}`);
+        // console.log(`- Last name: ${this.lastName}`);
     }
 }
-
+// Teacher (con) la mot truong hop dac biet cua Person (cha)
 class Teacher extends Person {
     constructor(firstName, lastName, subject) {
         super(firstName, lastName);
@@ -20,14 +21,19 @@ class Teacher extends Person {
     }
 
     print() {
+        console.log("Con");
         super.print();
         console.log(`- Subject: ${this.subject}`);
+    }
+
+    test() {
+        console.log(this.firstName);
     }
 }
 
 function main() {
-    const s = new Teacher('Bao', 'Vu', 'JavaScript');
-    s.print();
+    const t = new Teacher('Bao', 'Vu', 'JavaScript');
+    t.print();
 }
 
 main();
---
+++ demo/b14-15/oop/05-inheritance/hinh.js
@@ -0,0 +1,40 @@
+const readline = require('readline-sync');
+
+class HinhChuNhat {
+    constructor(chieuDai = 2, chieuRong = 1) {
+        this.chieuDai = chieuDai;
+        this.chieuRong = chieuRong;
+    }
+
+    nhap() {
+        this.chieuDai = Number(readline.question('Nhap chieu dai: '));
+        this.chieuRong = Number(readline.question('Nhap chieu rong: '));
+    }
+
+    tinhDienTich() {
+        return this.chieuDai * this.chieuRong;
+    }
+}
+
+class HinhVuong extends HinhChuNhat {
+    constructor(canh) {
+        super(canh, canh);
+    }
+
+    nhap() {
+        const canh = Number(readline.question("Nhap canh: "));
+        this.chieuDai = this.chieuRong = canh;
+    }
+}
+
+function main() {
+    const hcn = new HinhChuNhat();
+    hcn.nhap();
+    console.log(hcn.tinhDienTich());
+
+    const hv = new HinhVuong(4);
+    hv.nhap();
+    console.log(hv.tinhDienTich());
+}
+
+main();
